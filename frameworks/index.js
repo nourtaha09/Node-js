0b3e985d2307b2ffe4ca8110895dabd619c12aa3
nourtaha09 <102453078+nourtaha09@users.noreply.github.com>
2022-06-17 12:42:52 +0300

    Add files via upload

---
+++ frameworks/index.js
@@ -0,0 +1,39 @@
+
+// ((Koa))
+// pros:
+// -	Koa's small size means that it's very easy to learn and use
+// -	Koa is very flexible, allowing you to structure your code in whatever way you like
+// -	Koa has great support for async functions, making it easy to write code that is non-blocking
+// cons:
+// -	Koa is a relatively new framework, so there is less community support and documentation compared to Express
+// -	Koa does not include as many built-in features as Express, so you may need to use additional modules to get the functionality you need
+
+
+// ((Hapi)) 
+// pros:
+// -	Hapi is a very feature-rich framework, providing everything you need to build a production-ready API
+// -	Hapi is very well-documented, making it easy to get started with
+// -	Hapi has excellent support for plugins, allowing you to easily extend its functionality
+// cons:
+// -	Hapi can be a bit overwhelming for simple projects, as it includes so many features
+// -	Hapi's documentation can be confusing for newcomers, as it is sometimes difficult to find what you're looking for
+
+
+// ((Restify))
+// pros:
+// -	Restify is designed specifically for building APIs, so it includes everything you need to create a REST API
+// -	Restify is very easy to use, with a simple and straightforward API
+// -	Restify has excellent documentation, making it easy to get started with
+// cons:
+// -	Restify is not as flexible as some of the other frameworks, so it may not be suitable for all projects
+// -	Restify does not include as many features as some of the other frameworks, so you may need to use additional modules to get the functionality you need
+
+
+// ((Express)) 
+// pros:
+// -	Express is a very popular framework, so there is a lot of community support and documentation
+// -	Express is very flexible, allowing you to structure your code in whatever way you like
+// -	Express has a wide range of built-in features, so you don't need to use as many additional modules
+// cons:
+// -	Express can be a bit daunting for newcomers, as it is a very large and complex framework
+// -	Express' flexibility can also be a downside, as it can be easy to write code that is difficult to maintain
